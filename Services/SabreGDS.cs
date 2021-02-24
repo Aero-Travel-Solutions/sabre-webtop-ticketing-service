d528737c29b527ecde268720b31708f3fc2b2b6c
darshana.vithanage@aeronology.travel <darshana.vithanage@aeronology.travel>
2021-02-24 17:36:11 +1100

    Update

--- Services/SabreGDS.cs
+++ Services/SabreGDS.cs
@@ -2286,7 +2286,7 @@ namespace SabreWebtopTicketingService.Services
                 foreach (var conj in tktconjs)
                 {
                     logger.LogInformation($"Sector Countr: {conj.SectorCount}");
-                    int noofticketsallocated = Convert.ToInt32(Math.Round((decimal)(conj.SectorCount / 4), MidpointRounding.AwayFromZero)) -1;
+                    int noofticketsallocated = Convert.ToInt32(Math.Round((Convert.ToDecimal(conj.SectorCount) / 4M), MidpointRounding.AwayFromZero)) -1;
                     logger.LogInformation($"Conjunction Count: {noofticketsallocated}");
                     var selectedtkt = ticketdata.FirstOrDefault(f => f.DocumentType == "TKT" && f.PassengerName.StartsWith(conj.PassengerName));
                     if (selectedtkt != null)
