f4c62d928b733ae8f0afc65ed0d305452f73fb33
darshana.vithanage@aeronology.travel <darshana.vithanage@aeronology.travel>
2021-03-05 17:08:52 +1100

    FIX TICKETING ISSUE WITH CREDIT AMOUNT

---
+++ Interface/IQueueManagementDataSource.cs
@@ -0,0 +1,10 @@
+﻿using SabreWebtopTicketingService.Models;
+using System.Threading.Tasks;
+
+namespace SabreWebtopTicketingService.Interface
+{
+    public interface IQueueManagementDataSource
+    {
+        Task<MerchantLambdaResponse> RetriveCardData(string sessionId, string consolidatorid, string paymentsessionId, string orderid, decimal amount, string desc);
+    }
+}
