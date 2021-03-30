bc4061c91f56f5f73636f531386122dba7aeedf2
darshana.vithanage@aeronology.travel <darshana.vithanage@aeronology.travel>
2021-03-30 16:18:09 +1100

    update

--- Common/Logger.cs
+++ Common/Logger.cs
@@ -31,7 +31,7 @@ namespace SabreWebtopTicketingService.Common
 
 		public void LogMaskInformation(string messageTemplate, params object[] arguments)
         {
-			_logger.Information(messageTemplate.Mask(), arguments);
+			_logger.Information(messageTemplate.MaskNumber(), arguments);
 		}
 
 		public void LogError(string messageTemplate, params object[] arguments)
